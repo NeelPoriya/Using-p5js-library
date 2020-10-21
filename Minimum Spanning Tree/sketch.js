ac86a4aaafbedee79ca8a5e8daeba8e29ca27aca
Neel Poriya <64574792+NeelPoriya@users.noreply.github.com>
2020-10-21 11:05:28 +0530

    Add files via upload

---
+++ Minimum Spanning Tree/sketch.js
@@ -0,0 +1,62 @@
+var vertices = []
+
+function setup(){
+  createCanvas(640, 360)
+
+  var button = createButton('Reset')
+  button.style('display', 'block')
+  button.style('margin', '10px')
+  button.style('font-size', '200%')
+  button.mousePressed(resetSketch)
+}
+
+function resetSketch(){
+  vertices = []
+}
+
+function draw(){
+  background(50)
+  stroke(255)
+  strokeWeight(2)
+
+  var reached = []
+  var unreached = []
+
+  for(var i = 0; i < vertices.length; i++){
+    unreached.push(vertices[i])
+  }
+
+  reached.push(unreached[0])
+  unreached.splice(0,1)
+
+  while (unreached.length > 0) {
+    var record = 100000
+    var rIndex
+    var uIndex
+    for(var i = 0; i < reached.length; i++){
+      for(var j = 0; j < unreached.length; j++){
+        var v1 = reached[i]
+        var v2 = unreached[j]
+        var d = dist(v1.x, v1.y, v2.x, v2.y)
+        if(d < record){
+          record = d
+          rIndex = i
+          uIndex = j
+        }
+      }
+    }
+    line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y)
+    reached.push(unreached[uIndex])
+    unreached.splice(uIndex, 1)
+  }
+
+  for(var i = 0; i < vertices.length; i++){
+    ellipse(vertices[i].x, vertices[i].y, 10)
+  }
+}
+
+function mousePressed(){
+  var v = createVector(mouseX, mouseY)
+  if(v.x < width && v.y < height)
+    vertices.push(v)
+}
